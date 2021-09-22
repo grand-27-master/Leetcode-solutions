dd3278cfb782f5dd7af116ae5c2f5ce47876cef7
Vashishth  gajjar <vashishthkuntal.gajjar2019@vitstudent.ac.in>
2021-09-22 21:58:22 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (99.36%) - LeetHub

---
+++ richest-customer-wealth/richest-customer-wealth.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int max_wealth=0;
+        int n=accounts.size();
+        int m=accounts[0].size();
+        
+        for(int i=0;i<n;i++)
+        {
+            int sum=0;
+            for(int j=0;j<m;j++)
+            {
+                sum+=accounts[i][j];
+            }
+            max_wealth=max(max_wealth,sum);
+        }
+        return max_wealth;
+    }
+};
