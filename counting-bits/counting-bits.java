257c178d80ed6835d1bdd9dec8a7d4a22a5ffe7e
Vashishth  gajjar <vashishthkuntal.gajjar2019@vitstudent.ac.in>
2021-10-23 22:06:29 +0530

    Time: 7 ms (18.77%), Space: 45.1 MB (28.66%) - LeetHub

---
+++ counting-bits/counting-bits.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] res = new int[n + 1];
+        for (int i = 0; i < n + 1; ++ i) {
+            res[i] = countBitOne(i);
+        }
+        return res;
+        
+    }
+    
+    private int countBitOne(int num) {
+        int count = 0;
+        for (int i = 0; i < 32; ++ i) {
+            count += num & 1;
+            num >>= 1;
+        }
+        return count;
+    }
+}
