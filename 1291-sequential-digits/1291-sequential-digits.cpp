1158779e0360212c17ad4b127a0e6e5db678cd53
Vashishth  gajjar <vashishthkuntal.gajjar2019@vitstudent.ac.in>
2022-01-23 14:25:06 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (36.79%) - LeetHub

---
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+//       vector<int>ans;
+//         //int seq;
+        
+//         for(int i=low;i<=high;i++)
+//         {
+//             while(i>0)
+//             {
+                
+//             }
+//         }
+        
+//         sort(ans.begin(),ans.end());
+//         return ans;
+        
+        vector<int>arr{12,23,34,45,56,67,78,89,
+						123,234,345,456,567,678,789,
+						1234,2345,3456,4567,5678,6789,
+						12345,23456,34567,45678,56789,
+						123456,234567,345678,456789,
+						1234567,2345678,3456789,
+						12345678,23456789,
+						123456789};
+        
+        vector<int>ans;
+        int n=arr.size();
+        
+        for(int i=0;i<n;i++)
+        {
+            if(low<=arr[i] and arr[i]<=high)
+                ans.push_back(arr[i]);
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+    
+};
