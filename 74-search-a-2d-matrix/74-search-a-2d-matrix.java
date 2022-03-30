bc82c4441ed4b221c1d8697158ba621a097c9c04
Vashishth  gajjar <vashishthkuntal.gajjar2019@vitstudent.ac.in>
2022-03-30 09:43:16 +0530

    Time: 1 ms (32.47%), Space: 42.5 MB (60.33%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,13 +1,37 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i=0;i<matrix.length;i++)
+        
+        // LINEAR SEARCH: O(N^2);
+        
+        // for(int i=0;i<matrix.length;i++)
+        // {
+        //     for(int j=0;j<matrix[0].length;j++)
+        //     {
+        //         if(matrix[i][j]==target)
+        //             return true;
+        //     }
+        // }
+        // return false;
+        
+        
+        // BINARY SEARCH: O(LOGN)
+        
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int s=0,e=n*m-1;
+        
+        while(s<=e)
         {
-            for(int j=0;j<matrix[0].length;j++)
-            {
-                if(matrix[i][j]==target)
-                    return true;
-            }
+            int mid=s+(e-s)/2;
+            
+            if(target==matrix[mid/m][mid%m])
+                return true;
+            else if(matrix[mid/m][mid%m]>target)
+                e=mid-1;
+            else
+                s=mid+1;
         }
         return false;
     }
+    
 }
